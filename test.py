0ec7bb6b8e7e409fe6a9fe9fb9b397dc14905881
Will Stenzel <wstenzel@stu.naperville203.org>
2024-04-27 21:26:07 -0500

    bumper

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from vexcode import *
+from vexcode import triport
 
+
+DumperA = triport.DigitalPort('H')
 def main():
     drivetrain.drive_for(FORWARD, 100, MM)
-
+    if DumperA.pressed():
+        drivetrain.drive_for(Forward)
+        wait(5, SECONDS)
+        drivetrain.stop()
 vr_thread(main())
